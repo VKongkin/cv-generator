590687c218b5d8dfc85d747d2d57e393aaaa0821
VKongkin <voeun_kongkin@pp.bbu.edu.kh>
2025-07-25 16:04:12 +0700

    Update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -8,6 +8,7 @@ export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ["query", "info", "warn", "error"],
+    // The connection URL is set via DATABASE_PRISMA_DATABASE_URL in the environment
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
